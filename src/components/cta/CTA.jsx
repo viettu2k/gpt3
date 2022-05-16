c1432453d11747bfe5734550f9754489e9eee1c5
viettu2k <doanviettu2k@gmail.com>
2022-05-16 17:22:55 +0700

    cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import './cta.css';
+import './cta.scss';
 
 const CTA = () => {
-  return <div> CTA </div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta__btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
